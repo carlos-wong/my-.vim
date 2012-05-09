3ca25f7ae9ecd88b9cd69a15d6d6a47e8a3c4c97
carlos@192.168.8.216 <huaixian.huang@gmail.com>
2012-05-09 11:35:53 +0800

    base file

---
+++ auto_build_snk.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+PATH=/opt/mipsel-linux-uclibc/usr/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
+export PATH
+cd  /home/ugame_hhx/sofeware_workspace/auto_build_python_tool/
+/home/ugame_hhx/sofeware_workspace/auto_build_python_tool/auto_build.py /home/ugame_hhx/sofeware_workspace/auto_build_python_tool/app_rule.text 1 1
