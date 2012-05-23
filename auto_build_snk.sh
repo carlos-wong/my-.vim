89ce3bdf55dc5c4e8cb97c9af684db1031dc525a
carlos@192.168.8.216 <huaixian.huang@gmail.com>
2012-05-23 19:32:14 +0800

    back up

--- auto_build_snk.sh
+++ auto_build_snk.sh
@@ -3,5 +3,5 @@ PATH=/opt/mipsel-linux-uclibc/usr/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:
 export PATH
 cd  /home/ugame_hhx/sofeware_workspace/auto_build_python_tool/
 /home/ugame_hhx/sofeware_workspace/auto_build_python_tool/auto_build.py /home/ugame_hhx/sofeware_workspace/auto_build_python_tool/app_rule.text 1 1
-/home/ugame_hhx/sofeware_workspace/auto_build_python_tool/auto_build.py /home/ugame_hhx/sofeware_workspace/auto_build_python_tool/snk_4770_app.txt 1 1
+#/home/ugame_hhx/sofeware_workspace/auto_build_python_tool/auto_build.py /home/ugame_hhx/sofeware_workspace/auto_build_python_tool/snk_4770_app.txt 1 1
 
--- auto_build_snk_4770.sh
+++ auto_build_snk_4770.sh
@@ -2,5 +2,5 @@
 PATH=/opt/mipsel-linux-uclibc/usr/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games 
 export PATH
 cd  /home/ugame_hhx/sofeware_workspace/auto_build_python_tool/
-/home/ugame_hhx/sofeware_workspace/auto_build_python_tool/auto_build.py /home/ugame_hhx/sofeware_workspace/auto_build_python_tool/snk_4770_app.txt 1 0
+/home/ugame_hhx/sofeware_workspace/auto_build_python_tool/auto_build.py /home/ugame_hhx/sofeware_workspace/auto_build_python_tool/snk_4770_app.txt 1 1
 
