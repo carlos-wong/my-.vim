af825664624fe11864d433ca037ef2d3284d36dc
server_216 <server_216>
2012-06-15 14:54:07 +0800

    add l430 auto build sh

---
+++ auto_build_l430.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+PATH=/opt/mipsel-linux-uclibc/usr/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
+export PATH
+cd  /home/ugame_hhx/sofeware_workspace/auto_build_python_tool/
+/home/ugame_hhx/sofeware_workspace/auto_build_python_tool/auto_build.py /home/ugame_hhx/sofeware_workspace/auto_build_python_tool/l430_app.txt 1 1
+
