78340c70f8ed5f52e9fe8c5464374b0983bec1c3
server_216 <server_216>
2012-06-20 11:12:02 +0800

    echo something when build all

--- auto_build.sh
+++ auto_build.sh
@@ -14,8 +14,11 @@ then
     /home/ugame_hhx/auto_build_l430.sh 
   fi
 else
+  echo "build snk4770"
   /home/ugame_hhx/auto_build_snk_4770.sh > /home/ugame_hhx/share_snk_4770_burn/carlos_auto_build_4770.txt
+  echo "build snk 4760"
   /home/ugame_hhx/auto_build_snk.sh > /home/ugame_hhx/share_snk_burn/carlos_auto_build.txt
+  echo "build l430"
   /home/ugame_hhx/auto_build_l430.sh > /home/ugame_hhx/share_l430/carlos_auto_build.txt
 fi
 exit 0
