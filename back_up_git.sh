5b4f1e972d748534d5a6f7c51fdc79aaf177da72
server_216 <server_216>
2012-06-20 11:11:41 +0800

    move redmine sender to old place

--- back_up_git.sh
+++ back_up_git.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo tar jcfv  /home/ugame_hhx/redmine_backup/redmine_bakup_$(date +%Y%m%d-%H%M%S).tar.bz2 /usr/local/redmine-1.4.1 
 sudo tar jcfv  /home/ugame_hhx/git_backup/git_bakup_$(date +%Y%m%d-%H%M%S).tar.bz2 /opt/git/
-cd /usr/local/redmine-1.4.1/
-rake redmine:send_reminders RAILS_ENV="production" > /tmp/redmine_reminder.log
+#cd /usr/local/redmine-1.4.1/
+#rake redmine:send_reminders RAILS_ENV="production" > /tmp/redmine_reminder.log
 PATH=/opt/mipsel-linux-uclibc/usr/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
 export PATH
 /home/ugame_hhx/auto_clean_backup.py /home/ugame_hhx/git_backup > /home/ugame_hhx/git_backup/output.txt
