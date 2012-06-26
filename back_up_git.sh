2db273e7074c87abf557fec3657c7f7cde76f475
server_216 <server_216>
2012-06-26 12:41:24 +0800

    use auto clean tool in auto_build_python_tool

--- back_up_git.sh
+++ back_up_git.sh
@@ -5,5 +5,5 @@ sudo tar jcfv  /home/ugame_hhx/git_backup/git_bakup_$(date +%Y%m%d-%H%M%S).tar.b
 #rake redmine:send_reminders RAILS_ENV="production" > /tmp/redmine_reminder.log
 PATH=/opt/mipsel-linux-uclibc/usr/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
 export PATH
-/home/ugame_hhx/auto_clean_backup.py /home/ugame_hhx/git_backup > /home/ugame_hhx/git_backup/output.txt
-/home/ugame_hhx/auto_clean_backup.py /home/ugame_hhx/redmine_backup > /home/ugame_hhx/redmine_backup//output.txt
+/home/ugame_hhx/sofeware_workspace/auto_build_python_tool/auto_clean_pack.py /home/ugame_hhx/git_backup git_bakup_ > /home/ugame_hhx/git_backup/output.txt
+/home/ugame_hhx/sofeware_workspace/auto_build_python_tool/auto_clean_pack.py /home/ugame_hhx/redmine_backup redmine_bakup_ > /home/ugame_hhx/redmine_backup//output.txt
