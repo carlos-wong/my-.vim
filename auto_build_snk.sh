84bd38700b60707aecc2c1ef612e31024c7efb9d
carlos <huaixian.huang@gmail.com>
2012-09-05 10:22:38 +0800

    delete useless files

--- auto_build_snk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PATH=/opt/mipsel-linux-uclibc/usr/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
-export PATH
-cd  /home/ugame_hhx/sofeware_workspace/auto_build_python_tool/
-/home/ugame_hhx/sofeware_workspace/auto_build_python_tool/auto_build.py /home/ugame_hhx/sofeware_workspace/auto_build_python_tool/app_rule.text 1 1
-#/home/ugame_hhx/sofeware_workspace/auto_build_python_tool/auto_build.py /home/ugame_hhx/sofeware_workspace/auto_build_python_tool/snk_4770_app.txt 1 1
-/home/ugame_hhx/sofeware_workspace/auto_build_python_tool/auto_clean_pack.py /home/ugame_hhx/share_snk_burn/ SNK_4760_BURN_V
